73f555ac264e6d7c73e83106c4dbe3c646f00114
LucasSalesMoreira <lucas.sales@mc2performa.com.br>
2021-09-28 00:17:12 -0300

    Adicionando login ao controller de usuário e definindo sua rota

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -28,12 +28,31 @@ module.exports = {
                 icon_url
             });
             return res.status(200).json({ ok: true, user });
-        } catch (err) {
-            return res.status(400).json({ ok: false, error });
+        } catch (error) {
+            return res.status(400).json({ 
+                ok: false, 
+                error: 'O nick já existe!' 
+            });
         }
     },
 
-    async login() {
+    async login(req, res) {
+        try {
+            const { nick, password } = req.body;
+            const user = await User.findOne({
+                where: { nick, password }
+            });
+            
+            if (user) return res.status(200).json({ ok: true, user });
 
+            return res.status(400).json({ ok: false, error: 'Acesso negado!' });
+
+        } catch (error) {
+            console.error(error);
+            return res.status(400).json({ 
+                ok: false,
+                error: 'Algo deu errado, falha no processo de login!'
+            });
+        }
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const UserController = require('./controllers/UserController');
 routes.get('/user/getAll', UserController.getAll);
 routes.get('/user/get/:id', UserController.get);
 routes.post('/user/create', UserController.add);
+routes.post('/user/login', UserController.login);
 
 module.exports = routes;
