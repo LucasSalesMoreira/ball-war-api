cf29a6ea6bca163fcde0ce695a6836da3945eb5c
LucasSalesMoreira <lucas.sales@mc2performa.com.br>
2021-11-28 22:20:13 -0300

    Melhorando fluxo dos eventos

--- src/ioEvents.js
+++ src/ioEvents.js
@@ -1,9 +1,8 @@
+const GameController = require('./controllers/GameController');
 module.exports = (io) => io.on('connection', socket => { 
     console.log(`Client connected: ID -> ${socket.id}`);
 
-    socket.on('test', data => console.log(data));
-
-    socket.on('disconnect', ()=> {
-        console.log(`Client disconnected: ID -> ${socket.id}`);
-    });
+    socket.on('TO_ENTER', async (data) => await GameController.enter(io, { socket, data }));
+    
+    socket.on('disconnect', () => console.log(`Client disconnected: ID -> ${socket.id}`));
 });
