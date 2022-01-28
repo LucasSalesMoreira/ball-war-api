4d55ff11a145f0f8c1d10cbd6d7fe009725573d9
LucasSalesMoreira <lucasdevsoftware@gmail.com>
2022-01-27 23:36:50 -0300

    Melhorando controle de acesso aos mundos de jogo

--- src/controllers/GameController.js
+++ src/controllers/GameController.js
@@ -1,11 +1,19 @@
+let numberOfPlayers = [];
+
 module.exports = {
     async enter(io, client) {
         const { worldId } = client.data;
-        const roomName = `#room-${worldId}`;
-        const { socket } = client
-        socket.join(roomName);
-        await io.to(roomName).emit('NEW_PLAYER_IN_WORLD');
+        if (numberOfPlayers[worldId] < 4) {
+            const roomName = `#room-${worldId}`;
+            const { socket } = client
+            socket.join(roomName);
+            numberOfPlayers[worldId] += 1;
+            await io.to(roomName).emit('NEW_PLAYER_IN_WORLD');
+        } else {
+            socket.emit('CLOSED'); // Tratar no front-end!
+        }
     },
-    async start(io, client) {},
-    async finish(io, client) {}
+    async start(io, client) { },
+    async finish(io, client) { },
+    async exit(io, client) { }
 };
